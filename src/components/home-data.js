1392248efd7564cbb8cb4cacbf1bfd5b78eb8883
KAYOUNG KIM <lvyest.dev@gmail.com>
2024-11-21 16:05:34 +0900

    주석정리

--- src/components/home-data.js
+++ src/components/home-data.js
@@ -1,7 +1,7 @@
 const jobPostsData = [
     {
       title: "대우물산 지원",
-      date: "2024-10-01"
+      date: "2024-11-21"
 
     },
     {
@@ -9,11 +9,11 @@ const jobPostsData = [
       date: "2024-10-01"
     },
     {
-      title: "이플러스마트",
+      title: "현대산전",
       date: "2024-10-01"
     },
     {
-      title: "이플러스마트",
+      title: "두보식품",
       date: "2024-10-01"
     }
   ];
@@ -22,6 +22,10 @@ const jobPostsData = [
     {
       name: "책임감이 있는 직원, 김이음 입니다.",
       lastUpdated: "2024-09-28"
+    },
+    {
+      name: "맡겨주시면 열심히 일하겠습니다.",
+      lastUpdated: "2024-11-20"
     },
     {
       name: "맡겨주시면 열심히 일하겠습니다.",
